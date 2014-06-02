16fe14090df28b071095028537d26b905e034956
Tim S <stahta01@users.sourceforge.net>
2014-06-02 11:04:59 -0400

    Created patch to stop distribution of old wxWidgets templates; it still needs work and testing.

---
+++ Bat-Files/create-patchfile-CB_sf_tims_scriptedwizard.bat
@@ -0,0 +1,15 @@
+SET PATH=C:\Program Files\Git\bin;G:\GreenApps\GnuWin32\bin;%SystemRoot%\system32
+
+setlocal
+CD ..
+set TOP=%CD%
+
+CD ..\codeblocks_sf_tims_scriptedwizard
+
+set CB=%CD%
+
+CD %CB%
+
+git diff --diff-filter=M --no-prefix HEAD > %TOP%\Patches\Git\CB_sf_tims_scriptedwizard-patchfile.patch
+REM --ignore-space-at-eol --diff-filter= --ignore-space-change
+PAUSE
