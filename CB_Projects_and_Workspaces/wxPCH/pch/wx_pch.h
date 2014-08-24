296fc88e7058d01cf276ad1ad95daf3cfbe8e274
Tim S <stahta01@users.sourceforge.net>
2014-08-24 19:40:34 -0400

    Updated wxPCH project to define WX_PRECOMP in project and to undefine near in header file.

--- CB_Projects_and_Workspaces/wxPCH/pch/wx_pch.h
+++ CB_Projects_and_Workspaces/wxPCH/pch/wx_pch.h
@@ -6,15 +6,21 @@
 #ifndef WX_PCH_H_INCLUDED
 #define WX_PCH_H_INCLUDED
 
-#ifndef WX_PRECOMP
-    #define WX_PRECOMP
-#endif // USE_PCH
-
 // basic wxWidgets headers
 #include <wx/wxprec.h>
 
 #ifdef __BORLANDC__
     #pragma hdrstop
 #endif
+
+// Fix some of the windef.h define issues.
+#ifdef WIN32
+    #ifdef near
+        #undef near
+    #endif // #ifdef near
+    #ifdef far
+        #undef far
+    #endif // #ifdef far
+#endif // #ifdef WIN32
 
 #endif // WX_PCH_H_INCLUDED
