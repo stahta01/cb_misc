ea23ec7c13ffbbf62cdbedb4360981f317d5c928
Tim S <stahta01@users.sourceforge.net>
2014-09-09 10:15:12 -0400

    Redid the include layout to be a smaller change from current usage in CB.

--- Notes/sdk.cpp
+++ Notes/sdk.cpp
@@ -1,28 +1,49 @@
 // sdk.h must be the first include in the file to work as a PCH
 #include <sdk.h>
 
-// wxWidgets headers in both wx/wx.h and sdk_common.h
-#if !defined(CB_PRECOMP) && !defined(WX_PRECOMP)
+// wxWidgets headers in wx/wx.h; but, not in sdk_common.h
+#if !defined(WX_PRECOMP)
     // wxWidgets non GUI headers
+
+    // wxWidgets GUI headers
+//    #include <wx/settings.h>
+#endif // #if !defined(WX_PRECOMP)
+
+// CB SDK Headers in sdk_common.h
+#ifndef CB_PRECOMP
+    // wxWidgets non GUI headers in sdk_common.h
 //    #include <wx/app.h>
 //    #include <wx/arrstr.h>
 //    #include <wx/choicdlg.h>
+//    #include <wx/confbase.h>
+//    #include <wx/datetime.h>
+//    #include <wx/dir.h>
 //    #include <wx/dynarray.h>
 //    #include <wx/event.h>
+//    #include <wx/file.h>
+//    #include <wx/filename.h>
+//    #include <wx/fs_zip.h>
 //    #include <wx/hashmap.h>
 //    #include <wx/intl.h>
 //    #include <wx/list.h>
 //    #include <wx/log.h>
+//    #include <wx/process.h>
+//    #include <wx/regex.h>
+//    #include <wx/socket.h>
 //    #include <wx/string.h>
+//    #include <wx/thread.h>
 //    #include <wx/timer.h>
+//    #include <wx/txtstrm.h>
 //    #include <wx/utils.h>
+//    #include <wx/wfstream.h>
 
-    // wxWidgets GUI headers
+    // wxWidgets GUI headers in sdk_common.h
 //    #include <wx/bmpbuttn.h>
 //    #include <wx/button.h>
 //    #include <wx/checkbox.h>
 //    #include <wx/checklst.h>
 //    #include <wx/choice.h>
+//    #include <wx/colordlg.h>
 //    #include <wx/combobox.h>
 //    #include <wx/dcclient.h>
 //    #include <wx/dialog.h>
@@ -30,59 +51,36 @@
 //    #include <wx/font.h>
 //    #include <wx/frame.h>
 //    #include <wx/image.h>
+//    #include <wx/imaglist.h>
 //    #include <wx/listbox.h>
+//    #include <wx/listctrl.h>
 //    #include <wx/menu.h>
 //    #include <wx/menuitem.h>
 //    #include <wx/msgdlg.h>
+//    #include <wx/notebook.h>
 //    #include <wx/panel.h>
+//    #include <wx/print.h>
 //    #include <wx/radiobox.h>
 //    #include <wx/radiobut.h>
 //    #include <wx/sizer.h>
 //    #include <wx/slider.h>
+//    #include <wx/spinctrl.h>
+//    #include <wx/splitter.h>
 //    #include <wx/statbmp.h>
 //    #include <wx/stattext.h>
 //    #include <wx/textctrl.h>
 //    #include <wx/textdlg.h>
 //    #include <wx/toolbar.h>
-#endif // #if !defined(CB_PRECOMP) && !defined(WX_PRECOMP)
-
-// wxWidgets headers in sdk_common.h; but, not in wx/wx.h
-#if !defined(CB_PRECOMP)
-    // wxWidgets non GUI headers
-//    #include <wx/confbase.h>
-//    #include <wx/datetime.h>
-//    #include <wx/dir.h>
-//    #include <wx/file.h>
-//    #include <wx/filename.h>
-//    #include <wx/fs_zip.h>
-//    #include <wx/process.h>
-//    #include <wx/regex.h>
-//    #include <wx/socket.h>
-//    #include <wx/thread.h>
-//    #include <wx/txtstrm.h>
-//    #include <wx/wfstream.h>
-
-    // wxWidgets GUI headers
-//    #include <wx/colordlg.h>
-//    #include <wx/imaglist.h>
-//    #include <wx/listctrl.h>
-//    #include <wx/notebook.h>
-//    #include <wx/print.h>
-//    #include <wx/spinctrl.h>
-//    #include <wx/splitter.h>
 //    #include <wx/treectrl.h>
 //    #include <wx/xrc/xmlres.h>
-#endif // #if !defined(CB_PRECOMP)
 
-// CB SDK Headers in sdk_common.h
-#ifndef CB_PRECOMP
-    // basic headers
+    // CB SDK Headers in sdk_common.h
+//    #include <wx/wxscintilla.h>
+//    #include <editorcolourset.h>
 //    #include <settings.h>
 //    #include <globals.h>
 //    #include <sdk_events.h>
 //    #include <cbexception.h>
-
-    // absolute base classes
 //    #include <logger.h>
 //    #include <editorbase.h>
 //    #include <cbeditor.h>
@@ -97,12 +95,9 @@
 //    #include <compilerfactory.h>
 //    #include <compiler.h>
 //    #include <workspaceloader.h>
-//    #include <editorcolourset.h>
 //    #include <pipedprocess.h>
 //    #include <scrollingdialog.h>
 //    #include <cbauibook.h>
-
-    // managers
 //    #include <manager.h>
 //    #include <configmanager.h>
 //    #include <editormanager.h>
@@ -117,8 +112,6 @@
 //    #include <personalitymanager.h>
 //    #include <uservarmanager.h>
 //    #include <filemanager.h>
-
-    // other base files
 //    #include <xtra_res.h>
 //    #include <safedelete.h>
 //    #include <infowindow.h>
