7addc8497f4c4391e145b546cd7d8e7cafd429d7
Tim S <stahta01@users.sourceforge.net>
2014-10-09 10:03:55 -0400

    Added Bat-Files/create-Muliple-CB-Git-patches.bat

---
+++ Bat-Files/create-Muliple-CB-Git-patches.bat
@@ -0,0 +1,38 @@
+SET PATH=C:\Program Files\Git\bin;C:\GreenApps\GnuWin32\bin;%SystemRoot%\system32
+
+setlocal
+CD ..
+set TOP=%CD%
+
+CD /D C:\Users\stahta01\GitHome
+
+set GitHome=%CD%
+
+SET GIT_HEAD_COMMIT=5517a9c5df08016f1664074cfdf528f6f7286309
+
+cd %GitHome%\Production\codeblocks_mods2_plugin
+
+@echo off
+git diff --exit-code
+if errorlevel 1 (
+   echo Failure Reason Given is %errorlevel%
+   goto _ABORT
+)
+
+git diff --cached --exit-code
+if errorlevel 1 (
+   echo Failure Reason Given is %errorlevel%
+   goto _ABORT
+)
+
+@echo on
+
+git.exe checkout     plugin/ToolsPlus --
+
+git diff --diff-filter=M --no-prefix %GIT_HEAD_COMMIT% HEAD -- > %TOP%\Patches\Git\CB_mods2_plugin_ToolsPlus.patch
+PAUSE
+GOTO EOF
+
+:_ABORT
+echo "Operation Aborted"
+PAUSE
