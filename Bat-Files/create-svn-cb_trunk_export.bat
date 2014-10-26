cbf005975b656a2c78408a6c18658d39f3dba7ea
Tim S <stahta01@users.sourceforge.net>
2014-10-26 17:29:37 -0400

    Changed to "C:\Apps\GnuWin32\bin" folder instead of "C:\GreenApps\GnuWin32\bin" in setting the PATH.

--- Bat-Files/create-svn-cb_trunk_export.bat
+++ Bat-Files/create-svn-cb_trunk_export.bat
@@ -1,4 +1,4 @@
-SET PATH=C:\GreenApps\Subversion-1.7\bin;C:\GreenApps\GnuWin32\bin;%SystemRoot%\system32
+SET PATH=C:\GreenApps\Subversion-1.7\bin;C:\Apps\GnuWin32\bin;%SystemRoot%\system32
 
 setlocal
 CD ..
