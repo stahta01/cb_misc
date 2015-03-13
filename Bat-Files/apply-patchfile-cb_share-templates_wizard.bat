912a75247439112e1e48202cb2aeea67827a30e7
Tim S <stahta01@users.sourceforge.net>
2015-03-13 11:12:53 -0400

    Updated apply-patchfile-cb_obfuscated-templates_wizard.bat.

--- Bat-Files/apply-patchfile-cb_share-templates_wizard.bat
+++ Bat-Files/apply-patchfile-cb_share-templates_wizard.bat
@@ -4,6 +4,7 @@ setlocal
 
 CD C:\Users\stahta01\AppData\Roaming\codeblocks\share\codeblocks\templates\wizard
 
-patch --unified --strip=4 --ignore-whitespace --forward --input=C:/SourceCode/OpenSourceCode/Apps/IDE/Codeblocks/cb_misc-git/Patches/svn/cb_script_wxwidgets_phase1.patch
+REM patch --unified --strip=4 --ignore-whitespace --forward --input=C:/SourceCode/OpenSourceCode/Apps/IDE/Codeblocks/cb_misc-git/Patches/svn/cb_script_wxwidgets_phase1.patch
+patch --unified --strip=5 --ignore-whitespace --forward --input=C:/SourceCode/OpenSourceCode/Apps/IDE/Codeblocks/cb_misc-git/Patches/Git/cb_script_wxwidgets_phase2.diff
 
 PAUSE
