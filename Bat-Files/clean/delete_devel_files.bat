464dfb49a00cee98063fb837201206835d7b5165
Tim S <stahta01@users.sourceforge.net>
2015-03-14 12:52:34 -0400

    Added more files to be deleted.

--- Bat-Files/clean/delete_devel_files.bat
+++ Bat-Files/clean/delete_devel_files.bat
@@ -7,12 +7,60 @@ set DELETE_CMD=del
 set WX_VERSION_SUFFIX=%1
 REM set WX_VERSION_SUFFIX=30
 
-echo Cleaning files in the devel%WX_VERSION_SUFFIX% folders
+echo Deleting most files in the devel%WX_VERSION_SUFFIX% folders; does NOT clean Wizard Templates
 
-%DELETE_CMD% devel%WX_VERSION_SUFFIX%\codeblocks.exe
-%DELETE_CMD% devel%WX_VERSION_SUFFIX%\cb_console_runner.exe
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\*.exe
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\*.a
 %DELETE_CMD% devel%WX_VERSION_SUFFIX%\codeblocks.dll
-%DELETE_CMD% devel%WX_VERSION_SUFFIX%\libcodeblocks.a
-%DELETE_CMD% devel%WX_VERSION_SUFFIX%\libwxscintilla_cb.a
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxchartctrl.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxcustombutton.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxflatnotebook.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wximagepanel.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxkwic.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxled.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxpropgrid.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxsmithlib.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxspeedbutton.dll
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\wxtreelist.dll
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\tips.txt
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\*.zip
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.bmp
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.cbp
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.template
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.h
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.c
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\templates\*.cpp
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\SpellChecker\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\SpellChecker\OnlineSpellChecking.xml
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\scripts\*.script
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\scripts\*.gdb
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\plugins\*.dll
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\lib_finder\*.xml
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\lexers\*.xml
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\lexers\*.sample
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\16x16\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\codecompletion\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\codesnippets\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\DoxyBlocks\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\DoxyBlocks\16x16\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\settings\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\ThreadSearch\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\ThreadSearch\16x16\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\ThreadSearch\22x22\*.png
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\images\wxsmith\*.png
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\docs\index.ini
+
+%DELETE_CMD% devel%WX_VERSION_SUFFIX%\share\CodeBlocks\compilers\*.xml
 
 REM PAUSE
