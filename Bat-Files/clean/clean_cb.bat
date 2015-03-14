05758ff3993878ca0c478be356425e7f1eea58bd
Tim S <stahta01@users.sourceforge.net>
2015-03-14 12:57:03 -0400

    Updated clean/clean_cb.bat removed deleting files in devel folders.

--- Bat-Files/clean/clean_cb.bat
+++ Bat-Files/clean/clean_cb.bat
@@ -1,28 +1,27 @@
 set WX_VERSION_SUFFIX=
 set CB_OBJECT_DIR=.objs
 
-call clean_core_objs.bat %CB_OBJECT_DIR%
-    PAUSE
-call clean_PCH_objs.bat %CB_OBJECT_DIR%
-    PAUSE
-call clean_contrib_objs.bat %CB_OBJECT_DIR%
-    PAUSE
-call clean_misc_files.bat %WX_VERSION_SUFFIX%
-    PAUSE
-call clean_devel_files.bat %WX_VERSION_SUFFIX%
-    PAUSE
+    call clean_core_objs.bat %CB_OBJECT_DIR%
+REM PAUSE
+    call clean_PCH_objs.bat %CB_OBJECT_DIR%
+REM PAUSE
+    call clean_contrib_objs.bat %CB_OBJECT_DIR%
+REM PAUSE
+    call clean_misc_files.bat %WX_VERSION_SUFFIX%
+REM PAUSE
 
 
 set WX_VERSION_SUFFIX=30
 set CB_OBJECT_DIR=.objs30
 
-call clean_core_objs.bat %CB_OBJECT_DIR%
-    PAUSE
-call clean_PCH_objs.bat %CB_OBJECT_DIR%
-    PAUSE
-call clean_contrib_objs.bat %CB_OBJECT_DIR%
-    PAUSE
-call clean_misc_files.bat %WX_VERSION_SUFFIX%
-    PAUSE
-call clean_devel_files.bat %WX_VERSION_SUFFIX%
+    call clean_core_objs.bat %CB_OBJECT_DIR%
+REM PAUSE
+    call clean_PCH_objs.bat %CB_OBJECT_DIR%
+REM PAUSE
+    call clean_contrib_objs.bat %CB_OBJECT_DIR%
+REM PAUSE
+    call clean_misc_files.bat %WX_VERSION_SUFFIX%
+REM PAUSE
+
+
     PAUSE
