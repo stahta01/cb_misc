9061da8d8846a3dc7de63db91d574d2327c889d0
Tim S <stahta01@users.sourceforge.net>
2015-08-09 11:30:47 -0400

    Updated Bat-Files/apply-patchfile-cb_obfuscated-templates_wizard.bat

--- Bat-Files/apply-patchfile-cb_obfuscated-templates_wizard.bat
+++ Bat-Files/apply-patchfile-cb_obfuscated-templates_wizard.bat
@@ -1,10 +1,8 @@
 SET PATH=C:\Program Files\Git\bin;%SystemRoot%\system32
 
-setlocal
+CD C:\SourceCode\OpenSourceCode\Apps\IDE\Codeblocks\codeblocks_obfuscated-git
 
-CD C:\SourceCode\OpenSourceCode\Apps\IDE\Codeblocks\cb_obfuscated
-
-REM patch --unified --strip=0 --ignore-whitespace --forward --input=C:/SourceCode/OpenSourceCode/Apps/IDE/Codeblocks/cb_misc-git/Patches/svn/cb_script_wxwidgets_phase1.patch
+patch --unified --strip=0 --ignore-whitespace --forward --input=C:/SourceCode/OpenSourceCode/Apps/IDE/Codeblocks/cb_misc-git/Patches/svn/cb_script_wxwidgets_phase1.patch
 patch --unified --strip=1 --ignore-whitespace --forward --input=C:/SourceCode/OpenSourceCode/Apps/IDE/Codeblocks/cb_misc-git/Patches/Git/cb_script_wxwidgets_phase2.diff
 
 PAUSE
