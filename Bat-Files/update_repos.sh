293424359319a9b1399d0ca23cf3b7d2261658b5
Tim S <stahta01@gmail.com>
2015-11-01 19:02:49 -0500

    Added origin to the push statement; not yet tested it works.

--- Bat-Files/update_repos.sh
+++ Bat-Files/update_repos.sh
@@ -1,6 +1,6 @@
-cd ~/devel/version_control/codeblocks_svn2git_http_metadata-git && svn2git --rebase && git push --all
+cd ~/devel/version_control/codeblocks_svn2git_http_metadata-git && svn2git --rebase && git push origin --all
 
-cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && svn2git --rebase && git push --all
+cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && svn2git --rebase && git push origin --all
 
-cd ~/devel/version_control/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push --all
+cd ~/devel/version_control/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push origin --all
 
