f86f9a37908fa67f782f459f564a470623656670
Tim S <stahta01@gmail.com>
2015-11-15 09:40:07 -0500

    Added 13.12 and 15.xx branches to be pushed.

--- Bat-Files/update-codeblocks_https_metadata.sh
+++ Bat-Files/update-codeblocks_https_metadata.sh
@@ -1,2 +1,7 @@
 
 cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout master && git push downstream master
+
+cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout release-15.xx && git push downstream release-15.xx
+
+cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout release-13.12 && git push downstream release-13.12
+
