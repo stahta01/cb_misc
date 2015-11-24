24f9d3fc2d81831441a352510fa10a59c9ef819f
Tim S <stahta01@gmail.com>
2015-11-24 08:55:26 -0500

    Removed push to 13.12 branch.

--- Bat-Files/update-codeblocks_https_metadata.sh
+++ Bat-Files/update-codeblocks_https_metadata.sh
@@ -3,5 +3,4 @@ cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout
 
 cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout release-15.xx && git push downstream release-15.xx
 
-cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout release-13.12 && git push downstream release-13.12
 
