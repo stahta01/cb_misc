3dd1cbd5b9b5a6ac4178b39b3aba5309ef4a998c
Tim S <stahta01@users.sourceforge.net>
2015-11-28 22:23:14 -0500

    Added edited wx_pch.h file to notes folder.

---
+++ Notes/wx_pch.h
@@ -0,0 +1,19 @@
+#ifndef WX_PCH_H_INCLUDED
+#define WX_PCH_H_INCLUDED
+
+#if ( defined(USE_PCH) && !defined(WX_PRECOMP) )
+    #define WX_PRECOMP
+#endif // USE_PCH
+
+// basic wxWidgets headers
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#ifdef USE_PCH
+    // put here all your rarely-changing header files
+#endif // USE_PCH
+
+#endif // WX_PCH_H_INCLUDED
