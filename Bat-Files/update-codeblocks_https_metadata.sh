45c3493fedd8baf5f35ccd60d426aa56f2e1efa4
Tim S <stahta01@gmail.com>
2016-01-17 10:46:28 -0500

    Removed release-15.xx.

--- Bat-Files/update-codeblocks_https_metadata.sh
+++ Bat-Files/update-codeblocks_https_metadata.sh
@@ -1,6 +1,6 @@
 
 cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout master && git push downstream master
 
-cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout release-15.xx && git push downstream release-15.xx
+
 
 
