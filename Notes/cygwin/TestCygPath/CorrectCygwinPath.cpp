1a2e73361633c49265292052153c9a079b82aa8a
Tim S <stahta01@users.sourceforge.net>
2016-03-15 17:15:13 -0400

    Added code snippets from gdb_driver.cpp.

---
+++ Notes/cygwin/TestCygPath/CorrectCygwinPath.cpp
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Revision$
+ * $Id$
+ * $HeadURL$
+ */
+void CorrectCygwinPath(wxString& path)
+{
+    unsigned int i=0, EscCount=0;
+
+    // preserve any escape characters at start of path - this is true for
+    // breakpoints - value is 2, but made dynamic for safety as we
+    // are only checking for the CDprefix not any furthur correctness
+    if (path.GetChar(0)==g_EscapeChar)
+    {
+        while ( (i<path.Len()) && (path.GetChar(i)==g_EscapeChar) )
+        {
+            // get character
+            EscCount++;
+            i++;
+        }
+    }
+
+    // prepare to convert to a valid path if Cygwin is being used
+
+    // step over the escape characters
+    wxString PathWithoutEsc(path); PathWithoutEsc.Remove(0, EscCount);
+
+    if (PathWithoutEsc.StartsWith(m_CygdrivePrefix))
+    {
+        // remove cygwin prefix
+        if (m_CygdrivePrefix.EndsWith(_T("/"))) // for the case   "/c/path"
+          PathWithoutEsc.Remove(0, m_CygdrivePrefix.Len()  );
+        else                                    // for cases e.g. "/cygdrive/c/path"
+          PathWithoutEsc.Remove(0, m_CygdrivePrefix.Len()+1);
+
+        // insert ':' after drive label by reading and removing drive the label
+        // and adding ':' and the drive label back
+        wxString DriveLetter = PathWithoutEsc.GetChar(0);
+        PathWithoutEsc.Replace(DriveLetter, DriveLetter + _T(":"), false);
+    }
+
+    // Compile corrected path
+    path = wxEmptyString;
+    for (i=0; i<EscCount; i++)
+        path += g_EscapeChar;
+    path += PathWithoutEsc;
+}
---
+++ Notes/cygwin/TestCygPath/DetectCygwinMount.cpp
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Revision$
+ * $Id$
+ * $HeadURL$
+ */
+// routines to handle cygwin compiled programs on a Windows compiled C::B IDE
+void DetectCygwinMount(void)
+{
+    LONG lRegistryAPIresult;
+    HKEY hKey_CU;
+    HKEY hKey_LM;
+    TCHAR szCygwinRoot[BUFSIZE];
+    DWORD dwBufLen=BUFSIZE*sizeof(TCHAR);
+
+    // checking if cygwin mounts are present under HKCU
+    lRegistryAPIresult = RegOpenKeyEx( HKEY_CURRENT_USER,
+                         TEXT("Software\\Cygnus Solutions\\Cygwin\\mounts v2"),
+                         0, KEY_QUERY_VALUE, &hKey_CU );
+    if ( lRegistryAPIresult == ERROR_SUCCESS )
+    {
+        // try to readback cygwin root (might not exist!)
+        lRegistryAPIresult = RegQueryValueEx( hKey_CU, TEXT("cygdrive prefix"), NULL, NULL,
+                             (LPBYTE) szCygwinRoot, &dwBufLen);
+    }
+
+    // lRegistryAPIresult can be erroneous for two reasons:
+    // 1.) Cygwin entry is not present (could not be opened) in HKCU
+    // 2.) "cygdrive prefix" is not present (could not be read) in HKCU
+    if ( lRegistryAPIresult != ERROR_SUCCESS )
+    {
+        // Now check if probably present under HKLM
+        lRegistryAPIresult = RegOpenKeyEx( HKEY_LOCAL_MACHINE,
+                             TEXT("SOFTWARE\\Cygnus Solutions\\Cygwin\\mounts v2"),
+                             0, KEY_QUERY_VALUE, &hKey_LM );
+        if ( lRegistryAPIresult != ERROR_SUCCESS )
+        {
+            // cygwin definitely not installed
+            m_CygwinPresent = false;
+            return;
+        }
+
+        // try to readback cygwin root (now it really should exist here)
+        lRegistryAPIresult = RegQueryValueEx( hKey_LM, TEXT("cygdrive prefix"), NULL, NULL,
+                             (LPBYTE) szCygwinRoot, &dwBufLen);
+    }
+
+    // handle a possible query error
+    if ( (lRegistryAPIresult != ERROR_SUCCESS) || (dwBufLen > BUFSIZE*sizeof(TCHAR)) )
+    {
+        // bit of an assumption, but we won't be able to find the root without it
+        m_CygwinPresent = false;
+        return;
+    }
+
+    // close opened keys
+    RegCloseKey( hKey_CU ); // ignore key close errors
+    RegCloseKey( hKey_LM ); // ignore key close errors
+
+    m_CygwinPresent  = true;           // if we end up here all was OK
+    m_CygdrivePrefix = (szCygwinRoot); // convert to wxString type for later use
+}
