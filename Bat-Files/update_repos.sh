7c9983b6d097ecd3c7ca557218d09a790676ebed
Tim S <stahta01@gmail.com>
2016-04-20 11:08:25 -0400

    Commited out the last line.

--- Bat-Files/update_repos.sh
+++ Bat-Files/update_repos.sh
@@ -2,5 +2,5 @@ cd ~/devel/version_control/codeblocks_svn2git_http_metadata-git && svn2git --reb
 
 cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && svn2git --rebase && git push origin --all
 
-cd ~/devel/version_control/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push origin --all
+# cd ~/devel/version_control/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push origin --all
 
