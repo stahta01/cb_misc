a68022761d0c0a4ef43269a3435455ac12e41e1c
Tim S <stahta01@gmail.com>
2016-05-08 11:10:58 -0400

    * Updated local folder layout.

--- Bat-Files/update-codeblocks_https_metadata.sh
+++ Bat-Files/update-codeblocks_https_metadata.sh
@@ -1,5 +1,5 @@
 
-cd ~/devel/version_control/codeblocks_svn2git_https_metadata-git && git checkout master && git push downstream master
+cd ~/devel/open_source_code/version_control/codeblocks_svn2git_https_metadata-git && git checkout master && git push downstream master
 
 
 
