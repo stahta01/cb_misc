6fc8f9752044bd29ccfc4cec694b6f0a9633b752
Tim S <stahta01@users.sourceforge.net>
2016-12-10 12:20:38 -0500

    * Edited batch file to set wxUSE_REGKEY=0.

--- Bat-Files/Test_Build_wxMSW3.1.x_MSYS2_GCC6xx.bat
+++ Bat-Files/Test_Build_wxMSW3.1.x_MSYS2_GCC6xx.bat
@@ -12,13 +12,15 @@ REM Mingw GCC 6.x.x 32 bit
     SET _WXCFG=wx31X
     SET _WXCOMPILIER=MSys2_32bit
     SET _MINGWBASEFOLDER=C:\msys32\mingw32
+    SET _SEDBASEFOLDER=C:\msys32\usr
 REM SET _WXCXXFLAGS="-fno-strict-aliasing -fpermissive -Wno-deprecated-declarations -Wno-unused-local-typedefs -fno-keep-inline-dllexport"
 REM SET _WXCPPFLAGS="-DHAVE_TR1_TYPE_TRAITS"
 REM SET _WXCXXFLAGS="-Wno-deprecated-declarations -Wno-unused-local-typedefs -fno-keep-inline-dllexport"
 
 SET WXVENDOR=%_WXCOMPILIER%
 
-SET PATH=%_MINGWBASEFOLDER%\bin;C:\Program Files\Git\cmd;%SystemRoot%\system32
+SET PATH=%_MINGWBASEFOLDER%\bin;C:\Program Files\Git\cmd;%_SEDBASEFOLDER%\bin;%SystemRoot%\system32
+
 
 cd /d C:\Devel\open_source_code\version_control\wxWidgets-git
 REM PAUSE
@@ -28,15 +30,21 @@ REM PAUSE
 set _WXDIR=%CD%
 
 copy include\wx\msw\setup0.h include\wx\msw\setup.h
-copy include\wx\msw\setup0.h lib\gcc_dll%_WXCFG%\mswu\wx\setup.h
-copy include\wx\msw\setup0.h lib\gcc_lib%_WXCFG%\mswu\wx\setup.h
+
+REM sed --in-place "s/#   define wxUSE_GRAPHICS_CONTEXT 0/#   define wxUSE_GRAPHICS_CONTEXT 1/" include\wx\msw\setup.h
+sed --in-place "s/#define wxUSE_REGKEY 1/#define wxUSE_REGKEY 0/" include\wx\msw\setup.h
+
+if not exist gcc_mswu%_WXCFG% mkdir gcc_mswu%_WXCFG%
+if not exist gcc_mswudll%_WXCFG% mkdir gcc_mswudll%_WXCFG%
+copy include\wx\msw\setup.h lib\gcc_dll%_WXCFG%\mswu\wx\setup.h
+copy include\wx\msw\setup.h lib\gcc_lib%_WXCFG%\mswu\wx\setup.h
+
 
 cd %_WXDIR%\build\msw
 
 
 
 REM Static Builds
-if not exist gcc_mswu%_WXCFG% mkdir gcc_mswu%_WXCFG%
     ECHO "Start"  > gcc_mswu%_WXCFG%\builderr.txt
     ECHO "Clean"  > gcc_mswu%_WXCFG%\builderr.txt
     mingw32-make -f makefile.gcc BUILD=release LDFLAGS=%_WXLDFLAGS% CXXFLAGS=%_WXCXXFLAGS% CFLAGS=%_WXCFLAGS% CPPFLAGS=%_WXCPPFLAGS% CFG=%_WXCFG% VENDOR=%WXVENDOR% clean
@@ -46,7 +54,7 @@ if not exist gcc_mswu%_WXCFG% mkdir gcc_mswu%_WXCFG%
     ECHO "Finish" >> gcc_mswu%_WXCFG%\builderr.txt
 
 REM Shared/DLL Builds
-REM if not exist gcc_mswudll%_WXCFG% mkdir gcc_mswudll%_WXCFG%
+REM 
 
 REM ECHO "Start"  > gcc_mswudll%_WXCFG%\builderr.txt
 REM ECHO "Clean"  > gcc_mswudll%_WXCFG%\builderr.txt
