89e80298bad009d384211a4ade83d2b144a4500d
Tim S <stahta01@users.sourceforge.net>
2017-04-24 13:22:06 -0400

    * Added "Bat-Files/msys2_7zip-3.0.2-headers.sh".

---
+++ Bat-Files/msys2_7zip-3.0.2-headers.sh
@@ -0,0 +1,20 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+
+# Change to git folder
+cd ~/devel/open_source_code/version_control/wxWidgets_3_0_branch-git || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout wxwidgets3.0.2+git-lfs || exit 2
+# git status -uno
+
+
+mkdir -p wxWidgets-3.0.2_headers
+cp --recursive --update --preserve include wxWidgets-3.0.2_headers/
+rm -f wxWidgets-3.0.2_headers/include/wx/msw/setup.h
+
+7za u wxWidgets-3.0.2_headers.7z wxWidgets-3.0.2_headers\
+
+# echo "Finished"
