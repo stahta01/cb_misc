4200da108bc4e0f55099a37aae4dbbd16090e383
Tim S <stahta01@users.sourceforge.net>
2017-04-24 13:53:50 -0400

    * Added two files msys2_7zip-3.0.2-*.sh.

---
+++ Bat-Files/msys2_7zip-3.0.2-shared_monolib.sh
@@ -0,0 +1,19 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+
+# Change to git folder
+cd ~/devel/open_source_code/version_control/wxWidgets_3_0_branch-git || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout wxwidgets3.0.2+git-lfs || exit 2
+# git status -uno
+
+
+mkdir -p wxMSW-3.0.1_gcc630MSYS2_dllmono_Dev/lib
+cp --recursive --update --preserve lib/gcc630MSYS2_dllmono wxMSW-3.0.1_gcc630MSYS2_dllmono_Dev/lib/
+
+7za u wxMSW-3.0.1_gcc630MSYS2_dllmono_Dev.7z wxMSW-3.0.1_gcc630MSYS2_dllmono_Dev/
+
+# echo "Finished"
