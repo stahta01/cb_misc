55083b6ed10d019d79a149aecd0dac4244f412bc
Tim S <stahta01@users.sourceforge.net>
2017-04-23 20:49:31 -0400

    + Added "Bat-Files/gc_most_git_repos.sh".

---
+++ Bat-Files/gc_most_git_repos.sh
@@ -0,0 +1,15 @@
+cd ~/devel/open_source_code/version_control
+
+cd EmBlocks-git && git gc && \
+cd ../cb_misc-git && git gc && \
+cd ../cb_projects_for_wxWidgets-git && git gc && \
+cd ../codeblocks_PCH_fixes-git && git gc && \
+cd ../codeblocks_custom_build-git && git gc && \
+cd ../codeblocks_msys2-git && git gc && \
+cd ../codeblocks_r7456-git && git gc && \
+cd ../msys2_wx_config_exe-git && git gc && \
+cd ../wx-config-win-git && git gc && \
+cd ../wxTortoiseGit-git && git gc && \
+cd ../wxWidgets-2.8.12-git && git gc && \
+cd ../wxWidgets-git && git gc && \
+cd ../passwordsafe_git_code-git && git gc --auto
