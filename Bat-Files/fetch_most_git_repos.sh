e67db2c41ed901b8c25f888545e46cb638442a0c
Tim S <stahta01@gmail.com>
2017-04-25 12:02:49 -0400

    * Added exec and commented out windows only repos.

--- Bat-Files/fetch_most_git_repos.sh
+++ Bat-Files/fetch_most_git_repos.sh
@@ -5,10 +5,10 @@ cd ../cb_misc-git && git fetch --all --prune && \
 cd ../cb_projects_for_wxWidgets-git && git fetch --all --prune && \
 cd ../codeblocks_PCH_fixes-git && git fetch --all --prune && \
 cd ../codeblocks_custom_build-git && git fetch --all --prune && \
-cd ../codeblocks_msys2-git && git fetch --all --prune && \
 cd ../codeblocks_r7456-git && git fetch --all --prune && \
-cd ../msys2_wx_config_exe-git && git fetch --all --prune && \
-cd ../wx-config-win-git && git fetch --all --prune && \
+# cd ../codeblocks_msys2-git && git fetch --all --prune &&
+# cd ../msys2_wx_config_exe-git && git fetch --all --prune &&
+# cd ../wx-config-win-git && git fetch --all --prune &&
 cd ../wxTortoiseGit-git && git fetch --all --prune && \
 cd ../wxWidgets-2.8.12-git && git fetch --all --prune && \
 cd ../wxWidgets-git && git fetch --all --prune && \
