4db3cce0342d4a363a30ac41c7dd454a45cc28fd
Tim S <stahta01@users.sourceforge.net>
2017-04-26 10:45:02 -0400

    * Commented out lines in sh files.

--- Bat-Files/gc_most_git_repos.sh
+++ Bat-Files/gc_most_git_repos.sh
@@ -5,10 +5,10 @@ cd ../cb_misc-git && git gc && \
 cd ../cb_projects_for_wxWidgets-git && git gc && \
 cd ../codeblocks_PCH_fixes-git && git gc && \
 cd ../codeblocks_custom_build-git && git gc && \
-cd ../codeblocks_msys2-git && git gc && \
 cd ../codeblocks_r7456-git && git gc && \
-cd ../msys2_wx_config_exe-git && git gc && \
-cd ../wx-config-win-git && git gc && \
+# cd ../codeblocks_msys2-git && git gc &&
+# cd ../msys2_wx_config_exe-git && git gc &&
+# cd ../wx-config-win-git && git gc &&
 cd ../wxTortoiseGit-git && git gc && \
 cd ../wxWidgets-2.8.12-git && git gc && \
 cd ../wxWidgets-git && git gc && \
--- Bat-Files/prune_most_git_repos.sh
+++ Bat-Files/prune_most_git_repos.sh
@@ -5,10 +5,10 @@ cd ../cb_misc-git && git prune && \
 cd ../cb_projects_for_wxWidgets-git && git prune && \
 cd ../codeblocks_PCH_fixes-git && git prune && \
 cd ../codeblocks_custom_build-git && git prune && \
-cd ../codeblocks_msys2-git && git prune && \
 cd ../codeblocks_r7456-git && git prune && \
-cd ../msys2_wx_config_exe-git && git prune && \
-cd ../wx-config-win-git && git prune && \
+# cd ../codeblocks_msys2-git && git prune &&
+# cd ../msys2_wx_config_exe-git && git prune &&
+# cd ../wx-config-win-git && git prune &&
 cd ../wxTortoiseGit-git && git prune && \
 cd ../wxWidgets-2.8.12-git && git prune && \
 cd ../wxWidgets-git && git prune && \
