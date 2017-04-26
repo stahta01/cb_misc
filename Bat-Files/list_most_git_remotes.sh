8c8fc95b9bbfdaaadc0b49a723e3d494449d2523
Tim S <stahta01@users.sourceforge.net>
2017-04-25 23:42:48 -0400

    * Edited "Bat-Files/list_most_git_remotes.sh" to display all remotes.

--- Bat-Files/list_most_git_remotes.sh
+++ Bat-Files/list_most_git_remotes.sh
@@ -5,7 +5,7 @@ cd ../cb_misc-git && git remote -v && \
 cd ../cb_projects_for_wxWidgets-git && git remote -v && \
 cd ../codeblocks_PCH_fixes-git && git remote -v && \
 cd ../codeblocks_custom_build-git && git remote -v && \
-cd ../codeblocks_r7456-git && git remote get-url eb_ide && \
+cd ../codeblocks_r7456-git && git remote -v && \
 # cd ../codeblocks_msys2-git && git remote get-url cb_msys2 &&
 # cd ../msys2_wx_config_exe-git && git remote -v &&
 # cd ../wx-config-win-git && git remote -v &&
