e8d5c0697d83ef7f15eadab973ffada35e6e8d0d
Tim S <stahta01@users.sourceforge.net>
2017-04-26 19:14:18 -0400

    * Commented out some svn fetches.

--- Bat-Files/svn_fetch_git_repos.sh
+++ Bat-Files/svn_fetch_git_repos.sh
@@ -1,7 +1,7 @@
 cd ~/devel/open_source_code/version_control
 
 cd codeblocks_PCH_fixes-git && git svn fetch  && \
-cd ../codeblocks_custom_build-git && git svn fetch  && \
-cd ../codeblocks_console-git && git svn fetch  && \
-cd ../codeblocks_msys2-git && git svn fetch  && \
-cd ../codeblocks_wx_multilib_gcc471TDM-git && git svn fetch
+cd ../codeblocks_custom_build-git && git svn fetch
+# cd ../codeblocks_console-git && git svn fetch  && \
+# cd ../codeblocks_msys2-git && git svn fetch  && \
+# cd ../codeblocks_wx_multilib_gcc471TDM-git && git svn fetch
