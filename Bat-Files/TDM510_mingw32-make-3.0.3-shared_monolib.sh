48176d1e32b412d8a821f0743a11f0ef583b602f
Tim S <stahta01@users.sourceforge.net>
2017-05-12 13:14:20 -0400

    * Added "Bat-Files/TDM510_mingw32-make-3.0.3-shared_monolib.sh".

---
+++ Bat-Files/TDM510_mingw32-make-3.0.3-shared_monolib.sh
@@ -0,0 +1,38 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+
+# Change to git folder
+cd ~/devel/open_source_code/version_control/wxWidgets_3_0_branch-git || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout wxwidgets3.0.3+ || exit 2
+# git status -uno
+
+
+export PATH=/C/Apps32/MingwGCC510TDMmgw32/bin:$PATH
+
+# echo $PATH
+
+export _COMPILER_VERSION=510TDM
+
+# echo $_COMPILER_VERSION
+
+cd build/msw || exit 3
+
+mingw32-make -f makefile.gcc \
+  CFG=mono \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=debug
+
+mingw32-make -f makefile.gcc \
+  CFG=mono \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release
+
+# echo "Finished"
