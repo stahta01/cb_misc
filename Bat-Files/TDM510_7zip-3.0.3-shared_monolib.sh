166fe4fe5178b500d92247fa8c57a90c91cfa31d
Tim S <stahta01@users.sourceforge.net>
2017-05-15 03:30:09 -0400

    * Added "TDM510_7zip-3.0.3-shared_monolib.sh"

---
+++ Bat-Files/TDM510_7zip-3.0.3-shared_monolib.sh
@@ -0,0 +1,19 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+
+# Change to git folder
+cd ~/devel/open_source_code/version_control/wxWidgets_3_0_branch-git || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout wxwidgets3.0.3+git-lfs || exit 2
+# git status -uno
+
+
+mkdir -p wxMSW-3.0.3_gcc510TDM_dllmono_Dev/lib
+cp --recursive --update --preserve lib/gcc510TDM_dllmono wxMSW-3.0.3_gcc510TDM_dllmono_Dev/lib/
+
+7za u wxMSW-3.0.3_gcc510TDM_dllmono_Dev.7z wxMSW-3.0.3_gcc510TDM_dllmono_Dev/
+
+# echo "Finished"
