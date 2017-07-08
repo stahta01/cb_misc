b7f683d664df316b22df023caadcce8d7d256c42
Tim S <stahta01@users.sourceforge.net>
2017-07-08 11:43:57 -0400

    * Added "Bat-Files/status_most_git_repos.sh".

---
+++ Bat-Files/status_most_git_repos.sh
@@ -0,0 +1,12 @@
+cd ~/devel/open_source_code/version_control
+
+cd EmBlocks-git && git status && \
+cd ../cb_misc-git && git status && \
+cd ../cb_projects_for_wxWidgets-git && git status && \
+cd ../codeblocks_PCH_fixes-git && git status && \
+cd ../codeblocks_custom_build-git && git status && \
+cd ../codeblocks_r7456-git && git status && \
+cd ../wxTortoiseGit-git && git status && \
+cd ../wxWidgets-2.8.12-git && git status && \
+cd ../wxWidgets-git && git status && \
+cd ../passwordsafe_git_code-git && git status
