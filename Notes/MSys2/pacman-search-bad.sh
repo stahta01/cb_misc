2ef624e6d1c169362cfb5f6bf65aabc70173e575
Tim S <stahta01@users.sourceforge.net>
2017-09-15 11:12:17 -0400

    * Added pacman search scripts

---
+++ Notes/MSys2/pacman-search-bad.sh
@@ -0,0 +1,14 @@
+pacman -Ss mingw-w64-i686-arm-none-eabi-binutils
+pacman -Ss mingw-w64-i686-arm-none-eabi-gcc
+pacman -Ss mingw-w64-i686-arm-none-eabi-gdb
+pacman -Ss mingw-w64-i686-arm-none-eabi-newlib
+pacman -Ss mingw-w64-i686-assimp-git
+pacman -Ss mingw-w64-i686-atom-editor-git
+pacman -Ss mingw-w64-i686-atom-shell-git
+pacman -Ss mingw-w64-i686-attica-qt5
+
+pacman -Ss mingw-w64-i686-bc
+pacman -Ss mingw-w64-i686-binutils-git
+pacman -Ss mingw-w64-i686-bison
+pacman -Ss mingw-w64-i686-blender-git
+pacman -Ss mingw-w64-i686-btyacc
