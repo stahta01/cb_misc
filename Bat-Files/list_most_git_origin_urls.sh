a0fd7293bede607dd331ccc9bbea78db4d2b02cb
Tim S <stahta01@users.sourceforge.net>
2017-11-05 12:51:50 -0500

    Comment out repo that has no origin remote.

--- Bat-Files/list_most_git_origin_urls.sh
+++ Bat-Files/list_most_git_origin_urls.sh
@@ -5,7 +5,7 @@ cd ../cb_misc-git && git remote get-url origin && \
 cd ../cb_projects_for_wxWidgets-git && git remote get-url origin && \
 cd ../codeblocks_PCH_fixes-git && git remote get-url origin && \
 cd ../codeblocks_custom_build-git && git remote get-url origin && \
-cd ../codeblocks_r7456-git && git remote get-url origin && \
+# cd ../codeblocks_r7456-git && git remote get-url origin && \
 # cd ../codeblocks_msys2-git && git remote get-url cb_msys2 &&
 # cd ../msys2_wx_config_exe-git && git remote get-url origin &&
 # cd ../wx-config-win-git && git remote get-url origin &&
