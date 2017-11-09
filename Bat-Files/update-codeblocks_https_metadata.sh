fd9c2ce75b888c9acca6b003bd51bbd63e92b9e2
Tim S <stahta01@gmail.com>
2017-11-09 11:00:09 -0500

    Update folder name

--- Bat-Files/update-codeblocks_https_metadata.sh
+++ Bat-Files/update-codeblocks_https_metadata.sh
@@ -1,6 +1,3 @@
 
-cd ~/devel/open_source_code/version_control/codeblocks_svn2git_https_metadata-git && git checkout master && git push downstream master
-
-
-
+cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_https_metadata && git checkout master && git push downstream master
 
--- Bat-Files/update_repos.sh
+++ Bat-Files/update_repos.sh
@@ -1,6 +1,6 @@
-cd ~/devel/open_source_code/version_control/codeblocks_svn2git_http_metadata-git && svn2git --rebase && git push origin --all
+cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_http_metadata && svn2git --rebase && git push origin --all
 
-cd ~/devel/open_source_code/version_control/codeblocks_svn2git_https_metadata-git && svn2git --rebase && git push origin --all
+cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_https_metadata && svn2git --rebase && git push origin --all
 
-# cd ~/devel/open_source_code/version_control/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push origin --all
+# cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push origin --all
 
