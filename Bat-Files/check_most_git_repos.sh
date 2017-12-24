5b4791037e3e168494d62eec8a4e82a76ea42459
Tim S <stahta01@users.sourceforge.net>
2017-12-24 04:46:41 -0500

    Add/Edit "[full_]check_most_git_repos.sh"

--- Bat-Files/check_most_git_repos.sh
+++ Bat-Files/check_most_git_repos.sh
@@ -5,13 +5,11 @@ else
   cd $HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos
 fi
 
-cd cb_misc && git fsck && \
-cd ../cb_projects_for_wxWidgets && git fsck && \
-cd ../codeblocks_PCH_fixes && git fsck && \
-cd ../codeblocks_custom_build && git fsck && \
-cd ../codeblocks_r7456 && git fsck && \
-# cd ../msys2_wx_config_exe && git fsck &&
-# cd ../wx-config-win && git fsck &&
-cd ../wxTortoiseGit && git fsck && \
-cd ../wxWidgets && git fsck && \
-cd ../passwordsafe_git_code && git fsck
+cd cb_misc && git fsck --no-full && \
+cd ../cb_projects_for_wxWidgets && git fsck --no-full && \
+cd ../codeblocks_PCH_fixes && git fsck --no-full && \
+cd ../codeblocks_custom_build && git fsck --no-full && \
+cd ../codeblocks_r7456 && git fsck --no-full && \
+cd ../wxTortoiseGit && git fsck --no-full && \
+cd ../wxWidgets && git fsck --no-full && \
+cd ../passwordsafe_git_code && git fsck --no-full
---
+++ Bat-Files/full_check_most_git_repos.sh
@@ -0,0 +1,17 @@
+if [ -z "$HOMEDRIVE" ]
+then
+  cd ~/devel/open_source_code/version_control/git_repos
+else
+  cd $HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos
+fi
+
+cd cb_misc && git fsck && \
+cd ../cb_projects_for_wxWidgets && git fsck && \
+cd ../codeblocks_PCH_fixes && git fsck && \
+cd ../codeblocks_custom_build && git fsck && \
+cd ../codeblocks_r7456 && git fsck && \
+# cd ../msys2_wx_config_exe && git fsck &&
+# cd ../wx-config-win && git fsck &&
+cd ../wxTortoiseGit && git fsck && \
+cd ../wxWidgets && git fsck && \
+cd ../passwordsafe_git_code && git fsck
