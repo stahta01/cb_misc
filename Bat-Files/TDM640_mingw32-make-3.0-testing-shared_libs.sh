e179e3fc89743787766b241e7c29eca3450b6953
Tim S <stahta01@users.sourceforge.net>
2017-12-27 20:00:04 -0500

    Add "TDM640_mingw32-make-3.0-testing-shared_libs.sh"

---
+++ Bat-Files/TDM640_mingw32-make-3.0-testing-shared_libs.sh
@@ -0,0 +1,38 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+
+# Change to git folder
+cd ~/devel/open_source_code/version_control/git_repos/wxWidgets_3_0_branch || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout testing3.0 || exit 2
+# git status -uno
+
+
+export PATH=/C/GreenApps32/mingw32-TDM-gcc-6.4.0-2/bin:$PATH
+
+# echo $PATH
+
+export _COMPILER_VERSION=640TDM
+
+# echo $_COMPILER_VERSION
+
+cd build/msw || exit 3
+
+mingw32-make -f makefile.gcc \
+  CFG=testing3.0 \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=debug
+
+mingw32-make -f makefile.gcc \
+  CFG=testing3.0 \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=release
+
+# echo "Finished"
