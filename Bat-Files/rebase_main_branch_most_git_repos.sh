92354c8b8c73a3f295adf698bfa5cc5ff27a5fe9
Tim S <stahta01@users.sourceforge.net>
2018-01-06 10:08:21 -0500

    Edit rebase_main_branch_most_git_repos.sh

--- Bat-Files/rebase_main_branch_most_git_repos.sh
+++ Bat-Files/rebase_main_branch_most_git_repos.sh
@@ -5,6 +5,5 @@ else
   cd $HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos
 fi
 
-# cd codeblocks_custom_build && git checkout master && git fetch origin && git rebase origin/master
-cd wxTortoiseGit && git checkout master && git fetch upstream && git rebase upstream/master && \
-cd ../wxWidgets && git checkout master && git fetch upstream && git rebase upstream/master
+cd cb_misc && git checkout master && git pull origin master && \
+cd ../wxTortoiseGit && git checkout master && git pull upstream master 
