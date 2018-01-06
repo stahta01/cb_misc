c0ddb7cdaabc45b1b4e12e121ab1d8e05fed0bac
Tim S <stahta01@users.sourceforge.net>
2018-01-06 10:03:24 -0500

    Edit fetch_most_git_repos.sh

--- Bat-Files/fetch_most_git_repos.sh
+++ Bat-Files/fetch_most_git_repos.sh
@@ -10,9 +10,6 @@ cd ../cb_projects_for_wxWidgets && git fetch --all --prune && \
 cd ../codeblocks_PCH_fixes && git fetch --all --prune && \
 cd ../codeblocks_custom_build && git fetch --all --prune && \
 cd ../codeblocks_r7456 && git fetch --all --prune && \
-# cd ../codeblocks_msys2 && git fetch --all --prune &&
-# cd ../msys2_wx_config_exe && git fetch --all --prune &&
-# cd ../wx-config-win && git fetch --all --prune &&
 cd ../wxTortoiseGit && git fetch --all --prune && \
 cd ../wxWidgets && git fetch --all --prune && \
 cd ../passwordsafe_git_code && git fetch --prune ## --all errors out
