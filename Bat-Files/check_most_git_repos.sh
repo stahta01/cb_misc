7b42e643c7b3ffc1292fcad73c8a36cc9449065d
Tim S <stahta01@users.sourceforge.net>
2018-01-06 10:24:29 -0500

    Edit check_most_git_repos.sh

--- Bat-Files/check_most_git_repos.sh
+++ Bat-Files/check_most_git_repos.sh
@@ -5,11 +5,11 @@ else
   cd $HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos
 fi
 
-cd cb_misc && git fsck --no-full && \
-cd ../cb_projects_for_wxWidgets && git fsck --no-full && \
-cd ../codeblocks_PCH_fixes && git fsck --no-full && \
-cd ../codeblocks_custom_build && git fsck --no-full && \
-cd ../codeblocks_r7456 && git fsck --no-full && \
-cd ../wxTortoiseGit && git fsck --no-full && \
-cd ../wxWidgets && git fsck --no-full && \
-cd ../passwordsafe_git_code && git fsck --no-full
+cd cb_misc && git fsck --connectivity-only && \
+cd ../cb_projects_for_wxWidgets && git fsck --connectivity-only && \
+cd ../codeblocks_PCH_fixes && git fsck --connectivity-only && \
+cd ../codeblocks_custom_build && git fsck --connectivity-only && \
+cd ../codeblocks_r7456 && git fsck --connectivity-only && \
+cd ../wxTortoiseGit && git fsck --connectivity-only && \
+cd ../wxWidgets && git fsck --connectivity-only && \
+cd ../passwordsafe_git_code && git fsck --connectivity-only
