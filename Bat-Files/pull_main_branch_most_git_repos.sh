354075898d550206ad402035342c9f6eb7c9e6b2
Tim S <stahta01@users.sourceforge.net>
2018-01-07 11:29:13 -0500

    Delete scripts I moved to https://github.com/stahta01/git_notes_and_scripts

--- Bat-Files/pull_main_branch_most_git_repos.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$HOMEDRIVE" ]
-then
-  cd ~/devel/open_source_code/version_control/git_repos
-else
-  cd $HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos
-fi
-
-cd cb_misc && git checkout master && git pull origin master && \
-cd ../wxTortoiseGit && git checkout master && git pull upstream master 
