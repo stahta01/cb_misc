2938c133ad63d2384fb40d23ee59dfb70450b89d
Tim S <stahta01@gmail.com>
2018-05-28 03:09:27 -0400

    Add svn2git to shell script file.

--- Bat-Files/update-codeblocks_https_metadata.sh
+++ Bat-Files/update-codeblocks_https_metadata.sh
@@ -1,3 +1,4 @@
+cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_https_metadata && svn2git --rebase && git push origin --all
 
 cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_https_metadata && git checkout master && git push downstream master
 
