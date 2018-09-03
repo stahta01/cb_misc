56e76b3bf21a8ef1b396f6827ae364889d0b67c7
Tim S <stahta01@users.sourceforge.net>
2018-09-03 12:21:59 -0400

    Update example wx_pch.h

--- Notes/wx_pch.h
+++ Notes/wx_pch.h
@@ -1,6 +1,14 @@
 #ifndef WX_PCH_H_INCLUDED
 #define WX_PCH_H_INCLUDED
 
+#if defined(NOPCH) && defined(USE_PCH)
+    #undef USE_PCH
+#endif // NOPCH
+
+#if ( !defined(USE_PCH) && defined(WX_PRECOMP) )
+    #undef WX_PRECOMP
+#endif // !USE_PCH
+
 #if ( defined(USE_PCH) && !defined(WX_PRECOMP) )
     #define WX_PRECOMP
 #endif // USE_PCH
@@ -15,5 +23,12 @@
 #ifdef USE_PCH
     // put here all your rarely-changing header files
 #endif // USE_PCH
+
+// Fix windows define bugs not fixed in "wx/msw/winundef.h"
+#if defined(__WINDOWS__)
+    #ifdef near
+        #undef near
+    #endif
+#endif
 
 #endif // WX_PCH_H_INCLUDED
