66180f00292029683d6a34b0581825a289ea0979
Tim S <stahta01@users.sourceforge.net>
2018-09-10 20:32:52 -0400

    Delete "Bat-Files/update_repos.sh"

--- Bat-Files/update_repos.sh
+++
@@ -1,6 +0,0 @@
-# cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_http_metadata && svn2git --rebase && git push origin --all
-
-# cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_https_metadata && svn2git --rebase && git push origin --all
-
-# cd ~/devel/open_source_code/version_control/git_repos/codeblocks_svn2git_no_metadata-git && svn2git --rebase && git push origin --all
-
